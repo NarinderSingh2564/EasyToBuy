e098a9d489bf88e6887073b7cdfb678027463afb
NarinderSingh2564 <singhnarinder1102.com>
2024-04-08 16:50:08 +0530

    Peding Code

--- EasyToBuy.Models/SPResults/SPGetCartDetailsByCustomerId_Result.cs
+++ EasyToBuy.Models/SPResults/SPGetCartDetailsByCustomerId_Result.cs
@@ -18,12 +18,12 @@
         public int Id { get; set; }
         public string ProductShortName { get; set; } = string.Empty;
         public int ProductPrice { get; set; }
-        public decimal Discount { get; set; }
+        public decimal ProductDiscount { get; set; }
         public decimal ProductDiscountPrice { get; set; }
         public decimal ProductPriceAfterDiscount{ get; set; }
         public string ProductDescription { get; set; } = string.Empty;
         public string ProductImageUrl { get; set; } = string.Empty;
-        public string DelieveryTimeSpan { get; set; } = string.Empty;
+        public string DeliveryTimeSpan { get; set; } = string.Empty;
         public int Quantity { get; set; }
         public int TotalProductPrice { get; set; }
     }
