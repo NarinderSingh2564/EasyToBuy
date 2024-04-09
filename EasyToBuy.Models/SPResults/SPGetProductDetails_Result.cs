d87622e7f37438418fed513488f089aab3485e97
simar2903 <simarpreetk484@gmail.com>
2024-04-09 10:37:52 +0530

    final cart work

--- EasyToBuy.Models/SPResults/SPGetProductDetails_Result.cs
+++ EasyToBuy.Models/SPResults/SPGetProductDetails_Result.cs
@@ -6,7 +6,7 @@
         public string ProductShortName { get; set; } = string.Empty;
         public int ProductPrice { get; set; }
         public int ProductDiscount { get; set; }
-        public decimal PriceAfterDiscount { get; set; }
+        public decimal ProductPriceAfterDiscount { get; set; }
         public string ProductImageUrl { get; set; } = string.Empty;
         public string ProductDescription { get; set; } = string.Empty;
         public string PackingMode { get; set; } = string.Empty;
