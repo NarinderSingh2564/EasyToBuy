5d163f5550490ca832e74873c88b35cec1dd588e
arunkumar2553 <arunkumarbloom967@gmail.com>
2024-04-25 15:08:37 -0400

    image upload api

---
+++ EasyToBuy.Models/Models/ImageFileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace EasyToBuy.Models.Models
+{
+    public class ImageFileModel
+    {
+        public string fileName {  get; set; }
+        public IFormFile file { get; set; }
+    }
+}
---
+++ EasyToBuy.Web/Controllers/ImageUploaderController.cs
@@ -0,0 +1,36 @@
+﻿using EasyToBuy.Models.CommonModel;
+using EasyToBuy.Models.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyToBuy.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploaderController : ControllerBase
+    {
+        [HttpPost("ImageUpload")]
+        public ApiResponseModel ImageUpload([FromForm]ImageFileModel imageFileModel )
+        {
+            ApiResponseModel response = new ApiResponseModel();
+
+            try
+            {
+                string path = Path.Combine(@"C:\Users\admin\Project\Angular\EasyToBuyFrontEnd\src\assets\images", imageFileModel.fileName);
+                using(Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    imageFileModel.file.CopyTo(stream);
+                }
+                response.Status = true;
+                response.Message = "ImageUpload Successfully";
+            }
+            catch(Exception ex)
+            {
+                var msg = ex.Message;
+                response.Message = "Error occured please contact with developer";
+            }
+
+            return response;
+        }
+    }
+}
