0a50eb3e0d55ee3e79f189d815ba90209058d0ff
NarinderSingh2564 <singhnarinder1102.com>
2024-05-17 16:56:27 +0530

    Image In Prodduct

---
+++ EasyToBuy.Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using EasyToBuy.Data.DBClasses;
+using EasyToBuy.Data.SPClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyToBuy.Data
+{
+    public class ApplicationDbContext : DbContext   
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public DbSet<Address> tblAddress  { get; set; }
+        public DbSet<AddressType> tblAddressType { get; set; }
+        public DbSet<Vendor> tblVendor { get; set; }
+        public DbSet<User> tblUser { get; set; }
+        public DbSet<Category> tblCategory { get; set; }
+        public DbSet<Product> tblProduct { get; set; }
+        public DbSet<ProductWeights> tblProductWeight { get; set; } 
+        public DbSet<Cart> tblCart { get; set; }
+        public DbSet<OrderStatus> tblOrderStatus { get; set; }
+        public DbSet<Order> tblCustomerOrder { get; set; }     
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SG2NWPLS19SQL-v09.mssql.shr.prod.sin2.secureserver.net;Database=EasyToBuyDb;User Id=EasyToBuyAdmin; Password=Admin@2564;Trusted_Connection=False;TrustServerCertificate=true;Integrated Security=false;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+        }
+        public DbSet<SPGetCartDetailsByCustomerId_Result> cartDetailsByCustomerId_Results { get; set; }
+        public DbSet<SPGetProductList_Result> productList_Results { get; set; }
+        public DbSet<SPGetProductDetailsById_Result> productDetailsById_Results { get; set; }
+
+    }
+}
