b5d262f21fa246b41be5298ac7ab2c4246d93ca0
simar2903 <simarpreetk484@gmail.com>
2024-06-18 15:36:57 +0530

    Product Packing,Images and Variations add

---
+++ EasyToBuy.Data/DBClasses/ProductImages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyToBuy.Data.DBClasses
+{
+    public class ProductImages
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ProductVariationAndRate")]
+        public int VariationId { get; set; }
+        public virtual ProductVariationAndRate ProductVariationAndRate { get; set; }
+
+        [DataType("Varchar(50)")]
+        public string Image { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ EasyToBuy.Data/DBClasses/ProductPacking.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyToBuy.Data.DBClasses
+{
+    public class ProductPacking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DataType("Varchar(50)")]
+        public string PackingType { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
